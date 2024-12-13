2380c64b538448966dafe8163af3c991cbfbb2f9
Won Lee <wonseob2207@gmail.com>
2024-12-12 21:56:40 -0600

    Add files via upload

---
+++ practice/day_1.py
@@ -0,0 +1,18 @@
+# Day 1 | Won Lee
+# Task 1
+print("Hello World")
+# Task 2
+print("Hello "+"World"+"\nHello "+"World"+"\nHello "+"World")
+# Task 3
+print("Hello " + "" + input("What is your name?") + "!")
+# Task 4 
+    # pause 1
+print(len(input("What is your Name?")))
+    # pause 2
+username = input("What is your name?")
+length = len(username)
+print(length)
+# Task 5
+name = "Won"
+length = len(name)
+print(length)
---
+++ project/day_1_project.py
@@ -0,0 +1,6 @@
+# Day 1 Project | Band Name Generator Project | Won Lee
+print("Welcome to the Band Name Generator!")
+city_name = input("What is the city you grew up in?")
+pet_name = input("What is the name of your pet?")
+band_name = city_name + " " + pet_name
+print("Your band name is\n"+band_name)
