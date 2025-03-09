72cd4d7976383377a8107f82b7af88c71491168d
Won Lee <wonseob2207@gmail.com>
2025-03-09 10:59:30 -0600

    Add files via upload

---
+++ project/day_2_project.py
@@ -0,0 +1,11 @@
+# Program that calcualtes a tip from a bill and splits the bill
+# between n amount of people.
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+calc_tip = tip / 100
+final_tip = bill * calc_tip
+final_bill = (bill + final_tip) / people
+print(f"The final bill is {final_bill}")
