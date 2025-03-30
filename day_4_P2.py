1d514f194d9b2e19643cb25b6967e5f2250e3ef3
Won Lee <wonseob2207@gmail.com>
2025-03-30 16:54:46 -0600

    fifth commit for day_4.py

--- day_4_P2.py
+++ day_4_P2.py
@@ -3,6 +3,30 @@
 import random
 
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# Option One
 selected_friend = (random.choice(friends))
 
+print("OPTION ONE")
 print(f"The randomly chosen person is {selected_friend}")
+
+# Option Two
+random_choice = random.randint(0, 4)
+chosen_one = ""
+
+print("OPTION TWO")
+if random_choice == 0:
+    chosen_one = "Alice"
+    print(f"The randomly chosen person is {chosen_one}")
+elif random_choice == 1:
+    chosen_one = "Bob"
+    print(f"The randomly chosen person is {chosen_one}")
+elif random_choice == 2:
+    chosen_one = "Charlie"
+    print(f"The randomly chosen person is {chosen_one}")
+elif random_choice == 3:
+    chosen_one = "David"
+    print(f"The randomly chosen person is {chosen_one}")
+elif random_choice == 4:
+    chosen_one = "Emanuel"
+    print(f"The randomly chosen person is {chosen_one}")
