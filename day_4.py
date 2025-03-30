37414512271ad8716d726c9ae877ac78467bd765
Won Lee <wonseob2207@gmail.com>
2025-03-30 15:56:05 -0600

    init commit day_4.py

---
+++ day_4.py
@@ -0,0 +1,10 @@
+# Import modules
+import random
+import day_4_my_module
+
+random_integer = random.randint(1, 10)
+
+print(random_integer)
+
+# Print a variable from a module!
+print(day_4_my_module.my_favorite_number)
